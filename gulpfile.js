0d655af82898eff6d6d972f791772cde4231daed
Chris Davis <chris.davis5440@gmail.com>
2019-01-25 10:39:26 -0500

    editted gulpfile code to conform to gulp ver4

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
+// Compile sass into CSS
+function css(cb) {
   return gulp.src(['src/scss/main.scss'])
     .pipe(sass())
     .pipe(gulp.dest("src/css"));
-});
+  cb();
+};
 
 // Move the javascript files into our /src/js folder
-gulp.task('js', function () {
+function js(cb) {
   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js'])
     .pipe(gulp.dest("src/js"));
-});
+  cb();
+};
 
-gulp.task('default', gulp.series('sass', 'js'));
+gulp.task("css", css);
+gulp.task("js", js);
+
+gulp.task('default', gulp.series('css', 'js'));
