d495bea19990b28db514659dd8df90a0358d2b03
Chris Davis <chris.davis5440@gmail.com>
2019-01-25 12:47:52 -0500

    made sections at least 100vh also made fixed top navbar with logic to darken when scrolling down

---
+++ src/js/scripts.js
@@ -0,0 +1,12 @@
+$(function(){
+
+
+  $(window).scroll(() => {
+    if ($(window).scrollTop() > 500) {
+      $("#navbar").addClass("nav-scroll");
+    } else {
+      $("#navbar").removeClass("nav-scroll");
+    }
+  });
+
+});
